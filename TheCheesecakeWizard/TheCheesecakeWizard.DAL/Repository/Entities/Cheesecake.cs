a62adf686918c6dcb3fc261d3a21f809619a67be
Scott Denomme <scott.denomme@outlook.com>
2025-01-20 20:59:56 -0500

    Added Data Access Layer to Sln

---
+++ TheCheesecakeWizard/TheCheesecakeWizard.DAL/Repository/Entities/Cheesecake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCheesecakeWizard.DAL.Repository.Entities
+{
+    public class Cheesecake
+    {
+        private int _id;
+        public int Id 
+        { 
+            get => _id;
+            set { _id = value; } 
+        }
+
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}
---
+++ TheCheesecakeWizard/TheCheesecakeWizard.DAL/Repository/Entities/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCheesecakeWizard.DAL.Repository.Entities
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
