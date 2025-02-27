f9ab8d31f69fdd890bde538d91bafd2d8edb4adb
Ginger Code <sdenhof7@gmail.com>
2025-02-27 15:54:24 -0800

    Adding ingredients controller. (#1)
    * Pull- update
    
    * Sanity check.

--- TheCheesecakeWizard/TheCheesecakeWizard.DAL/Repository/Entities/Cheesecake.cs
+++ TheCheesecakeWizard/TheCheesecakeWizard.DAL/Repository/Entities/Cheesecake.cs
@@ -12,7 +12,8 @@ public class Cheesecake
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public decimal Price { get; set; }
+    public decimal Price { get; set; } 
+    
 
-    public ICollection<CheesecakeIngredient>? CheesecakeIngredients { get; set; } = new List<CheesecakeIngredient>();
+    public ICollection<CheesecakeIngredient> CheesecakeIngredients { get; set; } = new List<CheesecakeIngredient>();
 }
