77807065ce4c9f60b3d66f34412c6e1f5df4c628
Sthefani Denhof <sdenhof7@gmail.com>
2025-02-27 15:44:40 -0800

    Sanity check.

--- TheCheesecakeWizard/TheCheesecakeWizard.API/Controllers/ingredientsController.cs
+++ TheCheesecakeWizard/TheCheesecakeWizard.API/Controllers/ingredientsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TheCheesecakeWizard.DAL;
 using TheCheesecakeWizard.DAL.Repository.Entities;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Linq;
 
 namespace TheCheesecakeWizard.API.Controllers
 {
@@ -21,16 +23,17 @@ namespace TheCheesecakeWizard.API.Controllers
 
         // GET: api/Ingredients
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Ingredient>>> GetCheesecakeIngredient()
+        public async Task<ActionResult<IEnumerable<Ingredient>>> GetIngredients()
         {
             return await _context.Ingredients.ToListAsync();
         }
 
-        // GET: api/Ingredients/{id}
+        // GET: api/Ingredients/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Ingredient>> GetIngredient(int id)
         {
             var ingredient = await _context.Ingredients.FindAsync(id);
+
             if (ingredient == null)
             {
                 return NotFound();
@@ -39,8 +42,8 @@ namespace TheCheesecakeWizard.API.Controllers
             return ingredient;
         }
 
-
-        // PUT: api/Ingredients/{id}
+        // PUT: api/Ingredients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutIngredient(int id, Ingredient ingredient)
         {
@@ -70,19 +73,18 @@ namespace TheCheesecakeWizard.API.Controllers
             return NoContent();
         }
 
-
-
         // POST: api/Ingredients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Ingredient>> PostIngredient(Ingredient ingredient)
         {
             _context.Ingredients.Add(ingredient);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetIngredient), new { id = ingredient.Id }, ingredient);
+            return CreatedAtAction("GetIngredient", new { id = ingredient.Id }, ingredient);
         }
 
-        // DELETE: api/Ingredients/{id}
+        // DELETE: api/Ingredients/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteIngredient(int id)
         {
