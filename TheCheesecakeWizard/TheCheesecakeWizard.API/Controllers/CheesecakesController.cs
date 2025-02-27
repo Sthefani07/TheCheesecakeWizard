6981bbd1b33c601412e00bbb22ef407bbdf7c5bb
Sthefani Denhof <sdenhof7@gmail.com>
2025-02-27 15:28:06 -0800

    Pull- update

---
+++ TheCheesecakeWizard/TheCheesecakeWizard.API/Controllers/CheesecakesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheCheesecakeWizard.DAL;
+using TheCheesecakeWizard.DAL.Repository.Entities;
+
+namespace TheCheesecakeWizard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheesecakesController : ControllerBase
+    {
+        private readonly TheCheesecakeWizardDbContext _context;
+
+        public CheesecakesController(TheCheesecakeWizardDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cheesecakes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cheesecake>>> GetCheesecakes()
+        {
+            return await _context.Cheesecakes.ToListAsync();
+        }
+
+        // GET: api/Cheesecakes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cheesecake>> GetCheesecake(int id)
+        {
+            var cheesecake = await _context.Cheesecakes.FindAsync(id);
+
+            if (cheesecake == null)
+            {
+                return NotFound();
+            }
+
+            return cheesecake;
+        }
+
+        // PUT: api/Cheesecakes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCheesecake(int id, Cheesecake cheesecake)
+        {
+            if (id != cheesecake.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cheesecake).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CheesecakeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cheesecakes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cheesecake>> PostCheesecake(Cheesecake cheesecake)
+        {
+            _context.Cheesecakes.Add(cheesecake);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCheesecake", new { id = cheesecake.Id }, cheesecake);
+        }
+
+        // DELETE: api/Cheesecakes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCheesecake(int id)
+        {
+            var cheesecake = await _context.Cheesecakes.FindAsync(id);
+            if (cheesecake == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cheesecakes.Remove(cheesecake);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CheesecakeExists(int id)
+        {
+            return _context.Cheesecakes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TheCheesecakeWizard/TheCheesecakeWizard.API/Controllers/ingredientsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheCheesecakeWizard.DAL;
+using TheCheesecakeWizard.DAL.Repository.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TheCheesecakeWizard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly TheCheesecakeWizardDbContext _context;
+
+        public IngredientsController(TheCheesecakeWizardDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ingredients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ingredient>>> GetCheesecakeIngredient()
+        {
+            return await _context.Ingredients.ToListAsync();
+        }
+
+        // GET: api/Ingredients/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingredient>> GetIngredient(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return ingredient;
+        }
+
+
+        // PUT: api/Ingredients/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIngredient(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ingredient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IngredientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+
+        // POST: api/Ingredients
+        [HttpPost]
+        public async Task<ActionResult<Ingredient>> PostIngredient(Ingredient ingredient)
+        {
+            _context.Ingredients.Add(ingredient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetIngredient), new { id = ingredient.Id }, ingredient);
+        }
+
+        // DELETE: api/Ingredients/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIngredient(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ingredients.Remove(ingredient);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.Id == id);
+        }
+    }
+}
