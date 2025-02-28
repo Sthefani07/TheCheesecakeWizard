84509e719fb0bc15f09a7296a1841db8a6c2278c
Scott Denomme <scott.denomme@outlook.com>
2025-02-27 19:15:42 -0500

    Added Ingredient Service

---
+++ TheCheesecakeWizard/TheCheesecakeWizard.BL/Services/IngredientService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCheesecakeWizard.BL.Services.Interfaces;
+using TheCheesecakeWizard.DAL.Repository.Entities;
+
+namespace TheCheesecakeWizard.BL.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        public Task<Ingredient> CreateIngredientAsync(Ingredient ingredient)
+        {
+            throw new NotImplementedException();
+        }
+        public Task DeleteIngredientAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Ingredient>> GetAllIngredientsAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Ingredient> GetIngredientByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Ingredient> UpdateIngredientAsync(int id, Ingredient ingredient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
