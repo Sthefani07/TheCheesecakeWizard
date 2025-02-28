40ff4b8efba54763631200517d8f97144ce66a38
scottdenomme <75770997+scottdenomme@users.noreply.github.com>
2025-02-27 19:18:32 -0500

    Added Ingredient Service
    Added Ingredient Service

---
+++ TheCheesecakeWizard/TheCheesecakeWizard.BL/Services/Interfaces/IIngredientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCheesecakeWizard.DAL.Repository.Entities;
+
+namespace TheCheesecakeWizard.BL.Services.Interfaces;
+
+public interface IIngredientService
+{
+    Task<IEnumerable<Ingredient>> GetAllIngredientsAsync();
+    Task<Ingredient> GetIngredientByIdAsync(int id);
+    Task<Ingredient> CreateIngredientAsync(Ingredient ingredient);
+    Task<Ingredient> UpdateIngredientAsync(int id, Ingredient ingredient);
+    Task DeleteIngredientAsync(int id);
+}
